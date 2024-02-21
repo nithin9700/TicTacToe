26073d916d56e57526c7188d9f6d13124a464af5
NithinBharadwaj <76260241+nithin9700@users.noreply.github.com>
2024-02-21 15:34:56 +0530

    added some more service files

---
+++ service/EasyBotDifficultLevelStrategy.java
@@ -0,0 +1,11 @@
+package service;
+
+import Model.Board;
+import Model.Move;
+
+public class EasyBotDifficultLevelStrategy implements BotDifficultLevelStrategy{
+    @Override
+    public Move selectMove(Board board) {
+        return null;
+    }
+}
